db223e8ab011e65a9d40796c49644bf5dcea7696
JoshuaGGG <joshua.guzman@student.cart.org>
2023-02-02 10:44:08 -0800

    worked more

--- EECU Calculator/script.js
+++ EECU Calculator/script.js
@@ -3,32 +3,16 @@ const careerList = document.querySelectorAll('.career');
 
 var selectedCareer = document.querySelector(".career[data-value='$34,650-$147,000']");
 
-
-
-
-var careerValue = selectedCareer.dataset.value;
-
-
-
-
-var federalTaxes = careerValue * 0.12;
-var stateTaxes = careerValue * 0.07;
-var socialSecurity = careerValue * 0.062;
-var medicare = careerValue * 0.0145;
-var stateDisability = careerValue * 0.01;
-var retirementInvestment = careerValue * 0.05;
-var medicareInsurance = 180.00;
-var totalDeductions = federalTaxes + stateTaxes + socialSecurity + medicare + stateDisability + retirementInvestment + medicareInsurance;
-var grossIncome = careerValue;
-var netIncome = careerValue - totalDeductions;
+let federalTaxes= document.querySelector(".federal-taxes-input").value 
 
 
 
 
-document.querySelector(".federal-taxes-input").value = federalTaxes;
-document.querySelector(".state-taxes-input").value = stateTaxes;
-document.querySelector(".social-security-input").value = socialSecurity;
-document.querySelector(".medicare-input").value = medicare;
+
+ let statetaxes=document.querySelector(".state-taxes-input").value,
+
+socialSecurity=document.querySelector(".social-security-input").value,
+medicare=document.querySelector(".medicare-input").value,
 document.querySelector(".state-disability-input").value = stateDisability;
 document.querySelector(".retirement-investment-input").value = retirementInvestment;
 document.querySelector(".medicare-insurance-input").value = medicareInsurance;
@@ -39,7 +23,6 @@ document.querySelector(".net-income-input").value = netIncome;
 
 careerList.forEach(career) 
     career.addEventListener('click'), (event) => {
-        var careerValue = event.target.dataset.value;
   var careerValue = event.target.dataset.value;
   var federalTaxes = careerValue * 0.12;
   var stateTaxes = careerValue * 0.07;
