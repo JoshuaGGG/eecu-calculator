1d209af899436a48f2a66c1bf0044df39888a327
JoshuaGGG <joshua.guzman@student.cart.org>
2023-02-04 17:53:29 -0800

    2/4/2023 5:51pm
    I went from a simple nav bar to a hero image. Worked on search bar, added effect to the career list and added a calculator. I need to move the calculator to the right of the pay period deductions as well as finish the js for the checkbook and finally renovate the footer.

--- EECU Calculator/script.js
+++ EECU Calculator/script.js
@@ -1,18 +1,61 @@
+let availableKeywords =[
+  'Chemist',
+  'Engineer',
+  'Architect',
+  'Chef',
+  'Doctor',
+  'Electrician',
+  'Veterinarian',
+  'Nurse',
+  'Military',
+];
+const resultsBox =document.querySelector(".result-box");
+const inputBox =document.getElementById("input-box");
+inputBox.onkeyup =function(){
+  let result =[];
+  let input =inputBox.value;
+  if(input.length){
+    result = availableKeywords.filter((keyword)=>{
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+  display(result);
+
+  if(!result.length){
+    resultsBox.innerHTML = '';
+  }
+}
+function display(result){
+  const content = result.map((list)=>{
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  });
+  resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+function selectInput(list){
+  inputBox.value = list.innerHTML;
+  resultsBox.innerHTML ='';
+}
 const careerList = document.getElementsByClassName('career');
 console.log(careerList);
 
+
 var selectedCareer = document.querySelector(".career[data-value='$34,650-$147,000']");
 
+
 federalTaxes=document.getElementById("federalText")
-statetaxes=document.getElementById("stateText")
+stateTaxes=document.getElementById("stateText")
 socialSecurity=document.getElementById("socialText"),
 medicareText=document.getElementById("medicareText"),
-stateDisability=document.getElementById("stateText"),
+stateDisability=document.getElementById("disabilityText"),
 retirementInvestment=document.getElementById("retirementText"),
-medicareInsurance=document.getElementById("insuranceText"), 
+medicareInsurance=document.getElementById("insuranceText"),
 totalDeductions=document.getElementById("totalText"),
+totalDeductions=document.getElementById("twoText")
 grossIncome=document.getElementById("grossText"),
 netIncome=document.getElementById("netText")
+
+
 
 
 for (let index = 0; index < careerList.length; index++) {
@@ -30,16 +73,21 @@ for (let index = 0; index < careerList.length; index++) {
   var medicare = monthlyIncome * 0.0145;
   medicareText.innerText=medicare.toFixed(2);
   var stateDisability = monthlyIncome * 0.01;
-  stateText.innerText=stateDisability.toFixed(2);
+  disabilityText.innerText=stateDisability.toFixed(2);
   var retirementInvestment = monthlyIncome * 0.05;
   retirementText.innerText=retirementInvestment.toFixed(2);
   var medicareInsurance = 180.00;
   insuranceText.innerText=medicareInsurance.toFixed(2);
   var totalDeductions = federalTaxes + stateTaxes + socialSecurity + medicare + stateDisability + retirementInvestment + medicareInsurance;
   totalText.innerText=totalDeductions.toFixed(2);
+  var totalDeductions = federalTaxes + stateTaxes + socialSecurity + medicare + stateDisability + retirementInvestment + medicareInsurance;
+  twoText.innerText=totalDeductions.toFixed(2);
   var grossIncome = careerValue;
   grossText.innerText=grossIncome.toFixed(2);
   var netIncome = careerValue - totalDeductions;
   netText.innerText=netIncome.toFixed(2);
 })
 }
+
+
+
